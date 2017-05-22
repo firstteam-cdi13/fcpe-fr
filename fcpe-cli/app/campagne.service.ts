cd5bf663aef0899b1a3752237cfca9b30af4b10d
David ETIENNE <david.etienne@nantes.imie.fr>
2017-05-22 11:47:17 +0200

    dav: fcpe-cli ecran ECR4a correction appel service fcpe-api

--- fcpe-cli/app/campagne.service.ts
+++ fcpe-cli/app/campagne.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Campagne } from './model/campagne';
 
 //import Observable
-import { Http, Response, Headers, RequestOptions, RequestMethod, RequestOptionsArgs } from '@angular/http';
+import { Http, Response, Headers, RequestOptions, RequestMethod, RequestOptionsArgs, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 // // Import RxJs required methods
 import 'rxjs/add/operator/map';
@@ -17,15 +17,18 @@ export class CampagneService {
   constructor(private http: Http) {
     this.headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
     this.options = new RequestOptions({ headers: this.headers }); // Create a request option
-
   }
 
-  public rechercher(): Observable<Campagne[]> {
+  public rechercher(nom,statut): Observable<Campagne[]> {
     let url = '/api/campagnes/listeCampagneFiltree';
-    //ou chemin relatif this.url = '/api/contacts';
 
+    let params : URLSearchParams = new URLSearchParams();
+    params.set('nom', nom);
+    params.set('statut', statut);
 
-    return this.http.get(url,JSON.stringify({statut: 1}))
+    this.options.search = params;
+
+    return this.http.get(url, this.options)
       .map((res: Response) => {
         console.log("res",res)
         //Transcodage de la liste de contacts en tableau d'objets Contact
@@ -55,9 +58,9 @@ export class CampagneService {
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
 
-  public supprimer() {
+  public supprimer(campagne : Campagne) {
     console.log("CLI: appel service supprimerCampagne");
-    let url = '/api/campagnes/supprimerCampagne';
+    let url = '/api/campagnes/' + campagne.id;
     return this.http.delete(url);
   }
 
--- fcpe-cli/app/recherche-campagne/recherche-campagne.component.ts
+++ fcpe-cli/app/recherche-campagne/recherche-campagne.component.ts
@@ -13,6 +13,8 @@ export class RechercheCampagneComponent implements OnInit {
   campagnes: Campagne[];
   selectionCampagne: Campagne;
 
+  campagneCriteres : Campagne;
+
   listeNomCampagne: Campagne[];
 
   constructor(private campagneService: CampagneService) {
@@ -22,6 +24,19 @@ export class RechercheCampagneComponent implements OnInit {
   ngOnInit() {
     console.log("ngOnInit");
     this.aide = { message: "aide écran ECR4a" };
+    this.campagneCriteres = new Campagne(null,'',null,null,null);
+    this.campagneCriteres.statut = 2;
+    
+    this.campagneService.listerNomCampagne().subscribe(
+      datas => {
+        this.listeNomCampagne = datas;
+      },
+      err => {
+        console.log(err);
+        console.log("liste nom campagne erreur");        
+        //this.erreur = { message: err };
+      });
+
   }
 
   // ngAfterViewChecked() {
@@ -32,7 +47,12 @@ export class RechercheCampagneComponent implements OnInit {
    * Rechercher les campagnes
    */
   public rechercher() {
-    this.campagneService.rechercher().subscribe(
+
+    if (this.campagneCriteres != null) {
+      console.log(this.campagneCriteres.nom + " - " + this.campagneCriteres.statut);
+    }
+
+    this.campagneService.rechercher(this.campagneCriteres.nom,this.campagneCriteres.statut).subscribe(
       campagnes => {
         this.campagnes = campagnes;
       },
@@ -44,12 +64,38 @@ export class RechercheCampagneComponent implements OnInit {
       });
   }
 
-  public supprimer() {
+  public demanderSuppression(campagne : Campagne){
+     console.log("demanderSuppression une campagne : nom=" + campagne.nom);
+     this.selectionCampagne = campagne;
+  }
+
+  public annuler() {
+     console.log("Annuler confirmation");
+     this.selectionCampagne = null;
+  }
+
+  public supprimer(campagne : Campagne) {
     console.log("Supprimer une campagne");
-    this.campagneService.supprimer().subscribe(
+
+    let index = 0;
+
+    for (let element of this.campagnes){
+      if (element.id == campagne.id) {
+        this.campagnes.splice(index,1);
+        break;
+      }
+      index++;
+    }
+
+    this.campagneService.supprimer(campagne).subscribe(
+      () => {
+        console.log("Suppression OK");
+      },
       err => {
+        console.log("Suppression KO");
         console.log(err);
-        this.erreur = { message: err };
+        this.erreur = { message: "Echec de la suppression de la campagne \"" + campagne.nom + "\"" };
+        this.rechercher();
       });
   }
   
