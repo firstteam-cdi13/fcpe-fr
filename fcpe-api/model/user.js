8015af4472326f8382cf18187bc79932f2ee4a5e
Erwan <erwanguy@gmail.com>
2017-05-29 17:06:12 +0200

    +Route Authenticate (Test integration broken)

---
+++ fcpe-api/model/user.js
@@ -0,0 +1,18 @@
+class User{
+
+    constructor(id,nom,prenom,est_actif,id_connexion,mot_de_passe,email,favoris,id_etablissement){
+        this.id
+        this.nom
+        this.prenom
+        this.est_actif
+        this.id_connexion
+        this.mot_de_passe
+        this.email
+        this.favoris
+        this.id_etablissement
+    }
+
+
+}
+
+module.exports = User
